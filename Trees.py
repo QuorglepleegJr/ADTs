e2410e95d527e8ff85703039676435e1022dfbd6
Benjamin <17wilsb@olab.org.uk>
2023-04-21 14:55:54 +0100

    Completed search, implementing expressions

--- Trees.py
+++ Trees.py
@@ -1,3 +1,5 @@
+from operator import *
+
 class TreeError(Exception):
 
     pass
@@ -16,17 +18,11 @@ class Node():
 
 class BinaryNode(Node):
 
-    def __init__(self, value, children=None):
+    def __init__(self, value=None):
 
-        super().__init__(value, children)
+        super().__init__(value)
 
-        if len(self._children) > 2:
-
-            raise TreeError("Cannot create binary node with >2 children")
-
-        while len(self._children) < 2:
-
-            self._children.append(None)
+        self._children = [None,None]
         
     def set_left_child(self, child):
 
@@ -59,25 +55,29 @@ class BinaryNode(Node):
 
 class BinaryTree():
 
-    def __init__(self, initial_state=None):
+    def __init__(self):
 
-        if initial_state is None:
+        self.root = None
+    
+    def from_array(array):
+
+        if array is None:
 
             return
         
         rows = 0
 
-        while 2**rows - 1 < len(initial_state):
+        while 2**rows - 1 < len(array):
 
             rows += 1
         
-        while len(initial_state) < 2**rows - 1:
+        while len(array) < 2**rows - 1:
 
-            initial_state.append(None)
+            array.append(None)
         
         nodes = [None]
 
-        for datum in initial_state:
+        for datum in array:
 
             nodes.append(BinaryNode(datum))
         
@@ -95,54 +95,162 @@ class BinaryTree():
                 nodes[2**row_index + value].set_left_child(left)
                 nodes[2**row_index + value].set_right_child(right)
         
-        self.root = nodes[1]
+        tree = BinaryTree()
+        tree.root = nodes[1]
+        
+        return tree
     
     def traverse(root, mode=0):
 
         if root is None: return
 
-        if mode == 0: BinaryTree.__pre_traverse(root)
-        if mode == 1: BinaryTree.__in_traverse(root)
-        if mode == 2: BinaryTree.__post_traverse(root)
+        if mode == 0: BinaryTree._pre_traverse(root)
+        if mode == 1: BinaryTree._in_traverse(root)
+        if mode == 2: BinaryTree._post_traverse(root)
     
-    def __pre_traverse(root):
+    def _pre_traverse(root):
 
         if root is None: return
 
         print(root.get_value())
-        BinaryTree.__pre_traverse(root.get_left_child())
-        BinaryTree.__pre_traverse(root.get_right_child())
+        BinaryTree._pre_traverse(root.get_left_child())
+        BinaryTree._pre_traverse(root.get_right_child())
 
     
-    def __in_traverse(root):
+    def _in_traverse(root):
 
         
         if root is None: return
 
-        BinaryTree.__in_traverse(root.get_left_child())
+        BinaryTree._in_traverse(root.get_left_child())
         print(root.get_value())
-        BinaryTree.__in_traverse(root.get_right_child())
+        BinaryTree._in_traverse(root.get_right_child())
 
-    def __post_traverse(root):
+    def _post_traverse(root):
 
         if root is None: return
 
-        BinaryTree.__post_traverse(root.get_left_child())
-        BinaryTree.__post_traverse(root.get_right_child())
+        BinaryTree._post_traverse(root.get_left_child())
+        BinaryTree._post_traverse(root.get_right_child())
         print(root.get_value())
+
+class BinarySearchTree(BinaryTree):
+
+    def from_array(array):
+
+        nodes = []
+
+        for index in range(len(array)):
+
+            nodes.append(BinaryNode(array[index]))
+
+            if index > 0:
+
+                BinarySearchTree.add_new_child(nodes[0], nodes[index])
+        
+        tree = BinarySearchTree()
+        tree.root = nodes[0]
+
+        return tree
+
+    def add_new_child(origin, child):
+
+        if child.get_value() > origin.get_value():
+
+            if origin.get_right_child() is None:
+
+                origin.set_right_child(child)
+                return
+            
+            BinarySearchTree.add_new_child(origin.get_right_child(), child)
+        
+        else:
+
+            if origin.get_left_child() is None:
+
+                origin.set_left_child(child)
+                return
+            
+            BinarySearchTree.add_new_child(origin.get_left_child(), child)
+
+class BinaryExpressionTree(BinaryTree):
+
+    operators = {
+        "+":add,
+        "-":sub,
+        "*":mul,
+        "/":truediv,
+        "//":divmod
+    }
+
+    def from_string(string):
+
+        return BinaryExpressionTree.from_array(
+            BinaryExpressionTree._convert_rpn_string_to_array(string))
+    
+    def from_array(array):
+
+        return BinaryExpressionTree._from_rpn_array(array)
+
+    def _convert_rpn_string_to_array(string):
+
+        array = []
+
+        for element in string[1:-1].split(","):
+
+            
+    
+    def _from_rpn_array(array):
+
+        pass
+
+    def evaluate(inp):
+
+        if isinstance(inp, BinaryExpressionTree):
+
+            pass
+        
+        return BinaryExpressionTree._evaluate_array(inp)
+
+    def _evaluate_array(array):
+
+            stack = []
+
+            for element in array:
+
+                if callable(element):
+
+                    b = stack.pop()
+                    a = stack.pop()
+
+                    stack.append(element(a,b))
+                
+                else:
+
+                    stack.append(element)
+            
+            return stack.pop()
 
 
 if __name__ == "__main__":
 
-    with open("BinaryTreeTests.txt", "r") as tests:
+    print(BinaryExpressionTree.evaluate([9,4,add,3,2,mul,add]))
 
-        for test in tests.readlines():
+    # with open("BinaryTreeTests.txt", "r") as tests:
 
-            b = BinaryTree(eval(test))
+    #     for test in tests.readlines():
 
-            print(f"\nTest: {test}\n")
+    #         if test[0] == "b":
+                
+    #             b = BinarySearchTree.from_array(eval(test[1:]))
 
-            for i in range(3):
+    #         else:
 
-                BinaryTree.traverse(b.root, mode=i)
-                print()
+    #             b = BinaryTree.from_array(eval(test))
+
+    #         print(f"\nTest: {test}\n")
+
+    #         for i in range(3):
+
+    #             BinaryTree.traverse(b.root, mode=i)
+    #             print()
