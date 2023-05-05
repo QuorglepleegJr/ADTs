1d2371c08a8a482022154dd9f73450cfc92f54bb
= <=>
2023-05-05 18:41:11 +0100

    Added bfs

--- Graphs.py
+++ Graphs.py
@@ -1,6 +1,11 @@
+from Queues import LinearQueue
+from math import inf
+
 class Graph():
 
     def from_console():
+
+        # Takes input for directed, add both directions for undirected
 
         num = int(input("Enter number of vertices: "))
 
@@ -8,13 +13,13 @@ class Graph():
         
         edge_no = int(input("Enter number of edges: "))
 
-        if edge_no == num * (num-1):
+        if edge_no >= num * (num-1):
 
             print("Creating complete graph")
 
             return Graph.new_complete(num)
 
-        for x in range():
+        for x in range(edge_no):
 
             a = int(input(f"Edge {x+1} Start: "))
             b = int(input(f"Edge {x+1} End: "))
@@ -34,6 +39,33 @@ class Graph():
         self.__num_vertices = num_vertices
         self.__edges = set(edges)
     
+    def bfs(self, start=0):
+
+        adj_list = self.adj_list()
+
+        visit_q = LinearQueue(self.__num_vertices)
+        visited = [start]
+        distances = [inf] * self.__num_vertices
+        distances[start] = 0
+        parents = [None] * self.__num_vertices
+
+        visit_q.enqueue(start)
+
+        while not visit_q.is_empty():
+
+            current = visit_q.dequeue()
+
+            for other in adj_list[current]:
+                
+                if not other in visited:
+
+                    visited.append(other)
+                    visit_q.enqueue(other)
+                    distances[other] = distances[current] + 1
+                    parents[other] = current
+        
+        return visited, distances, parents
+
     def matrix(self):
 
         m = [[0]*self.__num_vertices for _ in range(self.__num_vertices)]
@@ -95,8 +127,42 @@ class Graph():
             print(*connections, sep=", ")
         
         print("\n")
+    
+    def print_bfs_order(self, start=0):
+
+        visited = self.bfs(start)[0]
+
+        print(", ".join([str(n) for n in visited]))
+    
+    def get_shortest_path(self, start=0, end=1):
+
+        distances, parents = self.bfs(start)
+
+        if distances[end] == inf:
+
+            print(f"No path exists between {start} and {end}")
+            print()
+
+            return
+
+        print(f"Shortest path between {start} and {end}:")
+        print(f"Length {distances[end]}")
+
+        path = []
+
+        current = end
+
+        while parents[current] is not None:
+
+            path = [current] + path
+            
+            current = parents[current]
+        
+        print(" -> ".join([str(n) for n in path]))
+        print()
+    
+        return distances[end], path
 
 g = Graph.from_console()
 
-g.print_list()
-g.print_matrix()
+g.get_shortest_path()
